c140797aeacab3b195de30b7826af6919d2ff9f1
MoffuGit <moffu.by@gmail.com>
2025-12-07 16:29:12 -0600

    WIP: is better?

--- src/watcher/fs.zig
+++ src/watcher/fs.zig
@@ -1,6 +1,8 @@
 const inotify = @import("fs/inotify.zig");
 const kqueue = @import("fs/kqueue.zig");
 const std = @import("std");
+const tree = @import("../tree.zig");
+const double = @import("../queue_double.zig");
 
 pub fn FileSystem(comptime xev: type) type {
     if (xev.dynamic) unreachable;
@@ -13,19 +15,92 @@ pub fn FileSystem(comptime xev: type) type {
     };
 }
 
-pub fn Callback(comptime T: type) type {
+pub fn FileWatcher(comptime xev: type) type {
+    return switch (xev.backend) {
+        .io_uring,
+        .epoll,
+        => struct {
+            const Self = @This();
+
+            wd: u32,
+
+            next: ?*Self = null,
+            rb_node: tree.IntrusiveField(Self) = .{},
+            completions: double.Intrusive(Completion) = .{},
+
+            pub fn compare(a: *Self, b: *Self) std.math.Order {
+                if (a.wd > b.wd) return .gt;
+                if (a.wd < b.wd) return .lt;
+                return .eq;
+            }
+        },
+        .kqueue => struct {
+            const Self = @This();
+
+            fd: i32 = -1,
+            c: xev.Completion = .{},
+
+            wd: u32,
+
+            next: ?*Self = null,
+            rb_node: tree.IntrusiveField(Self) = .{},
+            completions: double.Intrusive(Completion) = .{},
+
+            pub fn compare(a: *Self, b: *Self) std.math.Order {
+                if (a.wd > b.wd) return .gt;
+                if (a.wd < b.wd) return .lt;
+                return .eq;
+            }
+        },
+        else => unreachable,
+    };
+}
+
+pub const Completion = struct {
+    next: ?*Completion = null,
+    prev: ?*Completion = null,
+
+    userdata: ?*anyopaque = null,
+
+    callback: Callback() = NoopCallback(),
+
+    wd: u32 = 0,
+
+    flags: packed struct {
+        state: State = .dead,
+    } = .{},
+
+    const State = enum(u1) {
+        dead = 0,
+
+        active = 1,
+    };
+
+    pub fn state(self: Completion) CompletionState {
+        return switch (self.flags.state) {
+            .dead => .dead,
+            .active => .active,
+        };
+    }
+
+    pub fn invoke(self: *Completion, res: u32) CallbackAction {
+        return self.callback(self.userdata, self, res);
+    }
+};
+
+pub fn Callback() type {
     return *const fn (
         userdata: ?*anyopaque,
-        completion: *T.Completion,
+        completion: *Completion,
         result: u32,
     ) CallbackAction;
 }
 
-pub fn NoopCallback(comptime T: type) Callback(T) {
+pub fn NoopCallback() Callback() {
     return (struct {
         pub fn noopCallback(
             _: ?*anyopaque,
-            _: *T.Completion,
+            _: *Completion,
             _: u32,
         ) CallbackAction {
             return .disarm;
--- src/watcher/fs/kqueue.zig
+++ src/watcher/fs/kqueue.zig
@@ -5,12 +5,12 @@ const double = @import("../../queue_double.zig");
 const tree = @import("../../tree.zig");
 const pool = @import("../../pool.zig");
 const fspkg = @import("../fs.zig");
-const Callback = fspkg.Callback(@This());
+const Callback = fspkg.Callback();
 const CallbackAction = fspkg.CallbackAction;
 const CompletionState = fspkg.CompletionState;
-const NoopCallback = fspkg.NoopCallback(@This());
+const Completion = fspkg.Completion;
+const NoopCallback = fspkg.NoopCallback();
 const log = std.log.scoped(.fs);
-
 
 const CAPACITY = 100;
 const BASIS: u32 = 0x811c9dc5;
@@ -27,22 +27,15 @@ fn hash(bytes: []const u8) u32 {
 }
 
 pub fn FileSystem(comptime xev: type) type {
-    const WatcherPool = pool.Intrusive(FileWatcher(xev));
-
-
     return struct {
-        fn compare(a: *FileWatcher(xev), b: *FileWatcher(xev)) std.math.Order {
-            if (a.hash > b.hash) return .gt;
-            if (a.hash < b.hash) return .lt;
-            return .eq;
-        }
-
-        buffer: [CAPACITY]FileWatcher(xev) = undefined,
+        pub const FileWatcher = fspkg.FileWatcher(xev);
+        const WatcherPool = pool.Intrusive(FileWatcher);
+        buffer: [CAPACITY]FileWatcher = undefined,
 
         pool: WatcherPool = undefined,
-        tree: tree.Intrusive(FileWatcher(xev), compare) = .{},
+        tree: tree.Intrusive(FileWatcher, FileWatcher.compare) = .{},
         flags: packed struct {
-            init: bool =false,
+            init: bool = false,
         } = .{},
 
         const Self = @This();
@@ -55,7 +48,7 @@ pub fn FileSystem(comptime xev: type) type {
             if (self.flags.init) {
                 return;
             }
-                self.pool = WatcherPool.init(&self.buffer);
+            self.pool = WatcherPool.init(&self.buffer);
             self.flags.init = true;
         }
 
@@ -76,37 +69,36 @@ pub fn FileSystem(comptime xev: type) type {
         pub fn watch(self: *Self, loop: *xev.Loop, path: []const u8, c: *Completion) !void {
             self.start();
 
-            var temp: FileWatcher(xev) = .{.hash = hash(path)};
-            if(self.tree.find(&temp))|w| {
+            var temp: FileWatcher = .{ .wd = hash(path) };
+            if (self.tree.find(&temp)) |w| {
                 w.completions.push(c);
-            }else {
+            } else {
                 var w = try self.pool.alloc();
                 w.fd = try posix.open(path, posix.O{}, 0);
-                w.hash = temp.hash;
+                w.wd = temp.wd;
 
                 w.c = .{
                     .op = .{
                         .vnode = .{
                             .fd = w.fd,
-                            // Flags for various file system changes
                             .flags = std.c.NOTE.WRITE | std.c.NOTE.DELETE | std.c.NOTE.ATTRIB | std.c.NOTE.EXTEND | std.c.NOTE.LINK | std.c.NOTE.RENAME,
                         },
                     },
                     .userdata = w,
                     .callback = vnode_callback,
                 };
-                 loop.add(&w.c); // Add the kqueue vnode event to the main loop
+                loop.add(&w.c); // Add the kqueue vnode event to the main loop
 
                 self.tree.insert(w); // Add the FileWatcher to our tree
                 w.completions.push(c);
             }
 
-            c.hash = temp.hash;
+            c.wd = temp.wd;
             c.flags.state = .active;
         }
 
         pub fn cancel(self: *Self, c: *Completion) void {
-            var temp_wd = FileWatcher(xev) { .hash = c.hash };
+            var temp_wd = FileWatcher{ .wd = c.wd };
 
             if (self.tree.find(&temp_wd)) |watcher| {
                 watcher.completions.remove(c);
@@ -128,9 +120,9 @@ pub fn FileSystem(comptime xev: type) type {
             _: *xev.Completion, // The xev.Completion for this vnode watcher
             result: xev.Result,
         ) xev.CallbackAction {
-            const watcher: *FileWatcher(xev) = @ptrCast(@alignCast(ud.?));
+            const watcher: *FileWatcher = @ptrCast(@alignCast(ud.?));
             const vnode_flags = result.vnode catch |err| {
-                log.err("Vnode event error for fd {}: {any}", .{watcher.fd, err});
+                log.err("Vnode event error for fd {}: {any}", .{ watcher.fd, err });
                 // If there's an error on the vnode watcher itself, disarm it.
                 // All user completions for this watcher will effectively stop receiving events.
                 return .disarm;
@@ -164,71 +156,18 @@ pub fn FileSystem(comptime xev: type) type {
     };
 }
 
-pub fn FileWatcher(comptime xev: type) type {
-    return struct {
-        const Self = @This();
-
-        //shared file descriptor
-        fd: i32 = -1,
-        c: xev.Completion = .{},
-
-        //hashed path
-        hash: u32,
-
-        next: ?*Self = null,
-        rb_node: tree.IntrusiveField(Self) = .{},
-        completions: double.Intrusive(Completion) = .{},
-
-    };
-}
-
-pub const Completion = struct {
-    next: ?*Completion = null,
-    prev: ?*Completion = null,
-
-    userdata: ?*anyopaque = null,
-
-    callback: Callback = NoopCallback,
-
-    hash: u32 = 0,
-
-    flags: packed struct {
-        state: State = .dead,
-    } = .{},
-
-    const State = enum(u1) {
-        dead = 0,
-
-        active = 1,
-    };
-
-    pub fn state(self: Completion) CompletionState {
-        return switch (self.flags.state) {
-            .dead => .dead,
-            .active => .active,
-        };
-    }
-
-    pub fn invoke(self: *Completion, res: u32) CallbackAction {
-        return self.callback(self.userdata, self, res);
-    }
-};
-
-pub const Result = enum {};
-
 pub fn FileSystemTest(comptime xev: type) type {
     return struct {
         const testing = std.testing;
         const FS = FileSystem(xev);
 
-        test "test inotify file system watcher" {
+        test "test kqueue file system watcher" {
             var loop = try xev.Loop.init(.{});
             defer loop.deinit();
 
             var fs = try FS.init();
             defer fs.deinit();
 
-            // Test Case 1: Watch a single path with multiple completions
             const path1 = "test_path_1";
             _ = try std.fs.cwd().createFile(path1, .{});
             defer std.fs.cwd().deleteFile(path1) catch {};
@@ -240,8 +179,6 @@ pub fn FileSystemTest(comptime xev: type) type {
             try fs.watch(&loop, path1, &comp1_2);
             try fs.watch(&loop, path1, &comp1_3);
 
-            // // Test Case 2: Watch multiple distinct paths, each with one completion
-            //
             const path2 = "test_path_2";
             const path3 = "test_path_3";
             var comp2_1: Completion = .{};
@@ -255,7 +192,6 @@ pub fn FileSystemTest(comptime xev: type) type {
             try fs.watch(&loop, path2, &comp2_1);
             try fs.watch(&loop, path3, &comp3_1);
 
-            // Test Case 3: Watch multiple distinct paths, each with multiple completions
             const path4 = "test_path_4";
             const path5 = "test_path_5";
             _ = try std.fs.cwd().createFile(path4, .{});
@@ -285,7 +221,7 @@ pub fn FileSystemTest(comptime xev: type) type {
             try testing.expectEqual(fs.pool.countFree(), 97);
         }
 
-        test "test inotify polling" {
+        test "test kqueue vnode" {
             var loop = try xev.Loop.init(.{});
             defer loop.deinit();
 
@@ -321,13 +257,12 @@ pub fn FileSystemTest(comptime xev: type) type {
 
             _ = try loop.run(.once);
 
-            // Assert that the callback was invoked
             try testing.expectEqual(counter, 1);
 
             var counter2: usize = 0;
 
             var comp2: Completion = .{
-                .userdata = &counter2, // Pass the address of the counter
+                .userdata = &counter2,
                 .callback = custom_callback,
             };
 
@@ -336,12 +271,72 @@ pub fn FileSystemTest(comptime xev: type) type {
             _ = try file.write("hello");
             try file.sync();
 
-            // Run the event loop to process the inotify event
             _ = try loop.run(.no_wait);
 
-            // Assert that the callback was invoked
             try testing.expectEqual(counter, 2);
             try testing.expectEqual(counter2, 1);
+        }
+
+        test "test kqueue directory watcher" {
+            var loop = try xev.Loop.init(.{});
+            defer loop.deinit();
+
+            var fs = try FS.init();
+            defer fs.deinit();
+
+            const dir_path = "test_directory_kqueue";
+            try std.fs.cwd().makeDir(dir_path);
+            defer std.fs.cwd().deleteDir(dir_path) catch {};
+
+            const Event = struct { count: usize, flags: u32 };
+
+            var event = Event{ .count = 0, .flags = 0 };
+            const dir_callback_fn = struct {
+                fn invoke(ud: ?*anyopaque, _: *Completion, flags: u32) CallbackAction {
+                    const evt: *Event = @ptrCast(@alignCast(ud.?));
+                    evt.flags = flags;
+                    evt.count += 1;
+                    return .rearm;
+                }
+            }.invoke;
+
+            var comp: Completion = .{
+                .userdata = &event,
+                .callback = dir_callback_fn,
+            };
+
+            try fs.watch(&loop, dir_path, &comp);
+            _ = try loop.run(.no_wait);
+
+            try testing.expectEqual(event.count, 0);
+
+            const file1_path = dir_path ++ "/file1.txt";
+            _ = try std.fs.cwd().createFile(file1_path, .{});
+
+            _ = try loop.run(.once);
+            try testing.expectEqual(event.count, 1);
+            try testing.expectEqual(std.c.NOTE.WRITE, event.flags);
+
+            std.fs.cwd().deleteFile(file1_path) catch {};
+
+            _ = try loop.run(.once);
+            try testing.expectEqual(event.count, 2);
+            try testing.expectEqual(std.c.NOTE.WRITE, event.flags);
+
+            _ = try std.fs.cwd().createFile(file1_path, .{});
+            defer std.fs.cwd().deleteFile(file1_path) catch {};
+
+            _ = try loop.run(.once);
+            try testing.expectEqual(event.count, 3);
+            try testing.expectEqual(std.c.NOTE.WRITE, event.flags);
+
+            var file_handle = try std.fs.cwd().openFile(file1_path, .{ .mode = .read_write });
+            defer file_handle.close();
+            _ = try file_handle.write("some content");
+            try file_handle.sync();
+
+            _ = try loop.run(.no_wait);
+            try testing.expectEqual(event.count, 3);
         }
     };
 }
